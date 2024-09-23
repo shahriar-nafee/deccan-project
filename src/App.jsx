4216aa8e9211a7089f2300d007199bcd258ffda7
Shahriar Nafee <MSnafee@svam.com>
2024-09-23 17:26:14 +0600

    ADD: Responsive product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import { ProductCard } from "./components/ProductCard";
-import { data } from "./shared/data";
+import { data } from "./utils/data";
 
 function App() {
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("");
-  console.log(filter);
+
   return (
     <>
-      <div className="container">
-        <h3>data list</h3>
+      <div className="container my-3">
+        <h3>Product List</h3>
         <div className="my-3 row">
-          <div className="col-md-6">
+          <div className="col-md-4">
             <input
-              className="form-control"
+              className="form-control border-2"
               placeholder="Search Product by Name"
               type="search"
               name="search"
@@ -22,9 +22,9 @@ function App() {
               onChange={(e) => setSearch(e.target.value)}
             />
           </div>
-          <div className="col-md-6">
+          <div className="col-md-4 mt-3 mt-md-0 ms-auto">
             <select
-              className="form-select"
+              className="form-select border-2"
               onChange={(e) => setFilter(e.target.value)}
             >
               <option value="">All Products</option>
@@ -34,22 +34,29 @@ function App() {
             </select>
           </div>
         </div>
-        {data
-          ?.filter((item) => {
-            if (filter === "") {
-              return data;
-            } else {
-              return item.category.toLowerCase().includes(filter.toLowerCase());
-            }
-          })
-          ?.filter((data) =>
-            data.name.toLowerCase().includes(search.toLowerCase())
-          )
-          .map((item) => (
-            <div className="mt-3 w-25" key={item.name}>
-              <ProductCard item={item} />
-            </div>
-          ))}
+        <div className="row">
+          {data
+            ?.filter((item) => {
+              if (filter === "") {
+                return data;
+              } else {
+                return item.category
+                  .toLowerCase()
+                  .includes(filter.toLowerCase());
+              }
+            })
+            ?.filter((data) =>
+              data.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((item) => (
+              <div
+                className="mt-3 col-12 col-sm-6 col-md-4 col-lg-3"
+                key={item.name}
+              >
+                <ProductCard item={item} />
+              </div>
+            ))}
+        </div>
       </div>
     </>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,16 +2,14 @@ import PropTypes from "prop-types";
 
 export const ProductCard = ({ item }) => {
   return (
-    <div>
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{item.name}</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">
-            {item.category}
-          </h6>
-          <p className="card-text">{item.description}.</p>
-          <p className="card-text">{item.price}</p>
-        </div>
+    <div className="card h-100 shadow-sm">
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title">{item.name}</h5>
+        <h6 className="card-subtitle mb-2 text-black-50">{item.category}</h6>
+        <p className="card-text flex-grow-1">{item.description}</p>
+        <p className="card-text">
+          Price: <b>{item.price}</b>
+        </p>
       </div>
     </div>
   );
